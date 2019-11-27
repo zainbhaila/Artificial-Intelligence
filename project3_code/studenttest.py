45b469c691d6bcebbc4aa23ba42107537f3805b7
zainbhaila <zainbhaila@gmail.com>
2019-11-27 14:40:57 -0500

    testing

--- project3_code/studenttest.py
+++ project3_code/studenttest.py
@@ -5,14 +5,11 @@ from sklearn import datasets
 #import instrNet
 import studNet
 
+
 data = datasets.make_moons(n_samples=2500, noise=0.1)
-
-
 
 X = data[0]
 y = np.expand_dims(data[1], 1)
-
-
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, )
 
@@ -27,8 +24,6 @@ print(accuracy_score(y_test.T[0], results[0]))
 
 data = datasets.make_moons(n_samples=2500, noise=0.2)
 
-
-
 X = data[0]
 y = np.expand_dims(data[1], 1)
 
@@ -42,7 +37,36 @@ studentNet = studNet.test_train(xPass, yPass)
 results = studentNet.predict(x_test.T)
 print(accuracy_score(y_test.T[0], results[0]))
 
+
 data = datasets.make_circles(n_samples=500, noise=0.025)
+X = data[0]
+y = np.expand_dims(data[1], 1)
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, )
+
+xPass = x_train.T
+yPass = y_train.T
+
+studentNet = studNet.test_train(xPass, yPass)
+results = studentNet.predict(x_test.T)
+print(accuracy_score(y_test.T[0], results[0]))
+
+
+data = datasets.make_classification(n_samples=2500, n_features=4, n_informative=2, n_redundant=2)
+X = data[0]
+y = np.expand_dims(data[1], 1)
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, )
+
+xPass = x_train.T
+yPass = y_train.T
+
+studentNet = studNet.test_train(xPass, yPass)
+results = studentNet.predict(x_test.T)
+print(accuracy_score(y_test.T[0], results[0]))
+
+
+data = datasets.make_blobs(n_samples=2500, n_features=4, centers=2)
 X = data[0]
 y = np.expand_dims(data[1], 1)
 
